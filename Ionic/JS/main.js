7e27783034459998035e7e8051f00b0adbbcc229
Nikita_Developer_PMR <103760832+424Nkita-Csharsfta4@users.noreply.github.com>
2022-06-29 13:33:13 +0300

    Add files via upload

---
+++ Ionic/JS/main.js
@@ -0,0 +1,17 @@
+AOS.init();
+
+VANTA.NET({
+          el: "#Matrix",
+          mouseControls: true,
+          touchControls: true,
+          gyroControls: false,
+          minHeight: 200.00,
+          minWidth: 200.00,
+          scale: 1.00,
+          scaleMobile: 1.00,
+          color: 0xff55,
+          backgroundColor: 0x0,
+          points: 8.00,
+          maxDistance: 21.00,
+          spacing: 16.00
+        });
